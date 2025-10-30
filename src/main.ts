31f4866a6058e38cdfc0e3611f5981c2218d1071
m51k <mekail@gmail.com>
2025-10-31 01:32:30 +0530

    add persistent window state and app icon

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,41 @@
-import { app, components, BrowserWindow } from "electron";
+// import { app, components, BrowserWindow } from "electron";
+// import Store from "electron-store"
+// import * as path from 'path';
+const { app, components, nativeImage, BrowserWindow } = require("electron");
+const Store = require("electron-store");
+const path = require("path");
+const fs = require("fs")
+
+const store = new Store()
+// const trayIcon = nativeImage.createFromPath(path.join(__dirname, "../resources/icon.png")
+const appIcon = nativeImage.createFromPath(path.join(__dirname, "../resources/icon.png"))
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-		plugins: true,
-		webSecurity: true
-    }
-  });
+	const win = new BrowserWindow({
+		icon: appIcon,
+		width: (store.get('width') as number) || 1200,
+		height: (store.get('height') as number) || 800,
+		// TODO: omit these when undefined
+		// right now it opens in the corner
+		x: (store.get("x") as number) || 0,
+		y: (store.get("y") as number) || 0,
+		webPreferences: {
+			plugins: true,
+		}
+	});
 
-  mainWindow.loadURL("https://music.apple.com");
+	win.setMenuBarVisibility(false);
+	win.loadURL("https://music.apple.com");
+
+	function storeWindow() {
+		const [width, height] = win.getSize();
+		const [x, y] = win.getPosition();
+		store.set({ width, height, x, y });
+	}
+
+	win.on("close", () => {
+		storeWindow()
+	})
 }
 
 app.whenReady().then(async () => {
